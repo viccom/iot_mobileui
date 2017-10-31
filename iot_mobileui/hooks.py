a7fba9fda2a1983cc33f5697580c6f0a36a81714
viccom <3520845@qq.com>
2017-10-31 09:48:25 +0800

    new files

---
+++ iot_mobileui/hooks.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from . import __version__ as app_version
+
+app_name = "iot_mobileui"
+app_title = "Iot Mobileui"
+app_publisher = "viccom"
+app_description = "Iot Mobileui"
+app_icon = "oction octicon-device-mobile"
+app_color = "orange"
+app_email = "3520845@qq.com"
+app_license = "MIT"
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/iot_mobileui/css/iot_mobileui.css"
+# app_include_js = "/assets/iot_mobileui/js/iot_mobileui.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/iot_mobileui/css/iot_mobileui.css"
+# web_include_js = "/assets/iot_mobileui/js/iot_mobileui.js"
+
+# include js in page
+# page_js = {"page" : "public/js/file.js"}
+
+# include js in doctype views
+# doctype_js = {"doctype" : "public/js/doctype.js"}
+# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
+# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+#	"Role": "home_page"
+# }
+
+# Website user home page (by function)
+# get_website_user_home_page = "iot_mobileui.utils.get_home_page"
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Installation
+# ------------
+
+# before_install = "iot_mobileui.install.before_install"
+# after_install = "iot_mobileui.install.after_install"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "iot_mobileui.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+#	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"iot_mobileui.tasks.all"
+# 	],
+# 	"daily": [
+# 		"iot_mobileui.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"iot_mobileui.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"iot_mobileui.tasks.weekly"
+# 	]
+# 	"monthly": [
+# 		"iot_mobileui.tasks.monthly"
+# 	]
+# }
+
+# Testing
+# -------
+
+# before_tests = "iot_mobileui.install.before_tests"
+
+# Overriding Whitelisted Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "iot_mobileui.event.get_events"
+# }
+
